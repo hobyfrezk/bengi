a686c753f369b1de20ee47f103ad768372cf5d0b
chongshun <hobyfrezk@gmail.com>
2024-01-01 21:42:11 -0500

    add news model, fetch news endpoint and html fragments for news list

--- README.md
+++ README.md
@@ -22,4 +22,15 @@ handy command
 # create new app
 cd app
 python manage.py startapp appname
+
+# migrate db
+python app/manage.py makemigrations
+python app/manage.py migrate
+
+# create super user
+python app/manage.py createsuperuser
+
+# enter shell
+python app/manage.py shell
+
 ```
--- app/app/urls.py
+++ app/app/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("bengi_news.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+# this is required to serve media files in development
+# FIXME: need to fix when we push to prod
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
